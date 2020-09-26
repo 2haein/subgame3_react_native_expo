9706f4d60f4609b2eb259876dac1877216fbf88b
2haein <eehaein2@gmail.com>
2020-09-26 16:35:36 +0900

    push

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const createExpoWebpackConfig = require("@expo/webpack-config");
+
+module.exports = function(env, argv) {
+  env.mode = "development";
+  const config = createExpoWebpackConfig(env, argv);
+  return config;
+};
