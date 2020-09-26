b07638de3e9859bfcd4ee2e187cecb2bb68e1fdb
2haein <eehaein2@gmail.com>
2020-09-26 16:51:18 +0900

    index제거

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>app 추가하기</Text>
       <StatusBar style="auto" />
     </View>
   );
